63bdde4014ba640a6bc800fb846c2dd6d1bc4f9a
Tivlas <tima021003tima@gmail.com>
2022-06-22 17:46:50 +0300

    Made a test version of the main menu. We need to work out the design, add the Options section and more.

---
+++ Wake Up Project/Title screen/TitleScreen.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public class TitleScreen : Control
+{
+    public override void _Ready()
+    {
+        var first = GetNode<MarginContainer>("MarginContainer");
+        var second = first.GetNode<VBoxContainer>("VBoxContainer");
+        var third = second.GetNode<VBoxContainer>("Buttons");
+        var fourth = third.GetNode<MarginContainer>("Play");
+        var fifth = fourth.GetNode<Button>("PlayBtn");
+        fifth.GrabFocus();
+    }
+
+    public void _on_PlayBtn_button_up()
+    {
+        GetTree().ChangeScene("res://Core.tscn");
+    }
+
+    public void _on_OptionsBtn_button_up()
+    {
+
+    }
+
+    public void _on_AboutBtn_button_up()
+    {
+        GetTree().ChangeScene("res://Title screen/About the game/AboutScene.tscn");
+    }
+
+    public void _on_QuitBtn_button_up()
+    {
+        GetTree().Quit();
+    }
+}
