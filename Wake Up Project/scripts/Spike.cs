06b17a103564587f311b5566c2174d8d51f46a64
Tivlas <tima021003tima@gmail.com>
2022-08-03 09:36:29 +0300

    The is keyword can also assign the converted type to a variable if the cast works(since C# 7).

--- Wake Up Project/scripts/Spike.cs
+++ Wake Up Project/scripts/Spike.cs
@@ -1,16 +1,16 @@
 using Godot;
-using System;
 
 public class Spike : Node2D
 {
     public override void _Ready()
     {
-        
+
     }
 
-    private void _on_Area2D_body_entered(object body) {
-        if (body is PlayerController && body is KinematicBody2D) {
-            PlayerController playerController = body as PlayerController;
+    private void _on_Area2D_body_entered(object body)
+    {
+        if (body is PlayerController playerController && body is KinematicBody2D)
+        {
             playerController.TakeDamage();
         }
     }
