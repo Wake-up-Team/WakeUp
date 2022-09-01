44220a5a5e033a1fc9235f19163c2ba8004afd48
Tivlas <tima021003tima@gmail.com>
2022-09-02 00:11:27 +0300

    Removed coins from core scene. Created test version of burning log.

---
+++ Wake Up Project/scripts/Log.cs
@@ -0,0 +1,24 @@
+using Godot;
+
+public class Log : Area2D
+{
+    private AnimationPlayer _animationPlayer;
+
+    public override void _Ready()
+    {
+        _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+    }
+
+    private void _on_Log_body_entered(Node body)
+    {
+        if (body is PlayerController)
+        {
+            _animationPlayer.Play("burning");
+        }
+    }
+
+    private void _on_AnimationPlayer_animation_finished(string animationName)
+    {
+        QueueFree();
+    }
+}
