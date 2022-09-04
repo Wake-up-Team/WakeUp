77d0245f09390bbe8a060628672281fb24f5eea0
Tivlas <tima021003tima@gmail.com>
2022-09-04 16:16:43 +0300

    Created first version of door-elevator scene.

---
+++ Wake Up Project/scripts/Door.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+public class Door : Area2D
+{
+    private AnimationPlayer _animationPlayer;
+
+    private bool _theDoorIsOpen = false;
+    public override void _Ready()
+    {
+        _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+    }
+
+    public void _on_Door_body_entered(Node body)
+    {
+        if (body is PlayerController)
+        {
+            _animationPlayer.Play("open");
+            _theDoorIsOpen = true;
+        }
+    }
+
+    public void _on_Door_body_exited(Node body)
+    {
+        if (body is PlayerController)
+        {
+            _animationPlayer.PlayBackwards("open");
+            _theDoorIsOpen = false;
+        }
+    }
+
+    public override void _UnhandledInput(InputEvent inputEvent)
+    {
+        if (inputEvent is InputEventKey eventKey)
+        {
+            if (_theDoorIsOpen && eventKey.Pressed && eventKey.Scancode == (int)KeyList.J)
+            {
+                var sceneSwitcher = GetNode<SceneSwitcher>("/root/SceneSwitcher");
+                sceneSwitcher.SwitchScene("res://scenes/TitleScreen.tscn");
+            }
+        }
+    }
+}
